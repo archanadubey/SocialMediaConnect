ec9b7926ae41f4725e4a969888f7d34f508c9abd
Archana <archooworship@gmail.com>
2024-12-21 11:20:39 -0800

    utils Entity mapper and Dto mapper mapper committed

--- src/main/java/com/socialmediaconnect/app/service/impl/PostServiceImpl.java
+++ src/main/java/com/socialmediaconnect/app/service/impl/PostServiceImpl.java
@@ -1,13 +1,36 @@
 package com.socialmediaconnect.app.service.impl;
 
 import com.socialmediaconnect.app.dto.PostDto;
+import com.socialmediaconnect.app.service.PostServices;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-public class PostServiceImpl {
-
-
+@Service
+public class PostServiceImpl implements PostServices {
 
 
+    @Override
+    public List<PostDto> getAllPost() {
+        return null;
+    }
 
+    @Override
+    public PostDto getPostById(long id) {
+        return null;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postToBeCreated) {
+        return null;
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postToBeUpdated) {
+        return null;
+    }
+
+    @Override
+    public boolean deletePostByID(long idToBeDeleted) {
+        return false;
+    }
 }
